c93729e71430bc25e766aea90a73a45b88255d40
AGL\A136836 <amarambe@agl.com.au>
2017-02-02 15:48:09 +1100

    Added Redux dependency.

---
+++ app/components/TopMenu/topMenuIndex.jsx
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+
+const TopMenuComponent = () => (
+    <div className="nav">
+        <header role="banner">
+            <nav role='navigation'>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Catalog</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+)
+
+export default TopMenuComponent;
---
+++ app/containers/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {TopMenuComponent} from '../components/TopMenu/topMenuIndex';
+
+const App = () => (
+    <div>
+        <TopMenuComponent/>
+    </div>
+
+);
