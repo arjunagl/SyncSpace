30136d43a53e7ccf5bcd5bdae4ce47843e52aae2
AGL\A136836 <amarambe@agl.com.au>
2017-02-09 09:13:12 +1100

    Added Container.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 import TopMenuComponent from '../topMenu/TopMenuIndex';
-import LoginComponent from '../login/Login';
+// import LoginComponent from '../login/Login';
+import LoginContainer from '../login/LoginContainer';
 
 
 const App = () => (
     <div>
         <TopMenuComponent/>
-        <LoginComponent/>
+        <LoginContainer/>
     </div>
 );
 
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,16 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './Login.scss';
 
-const LoginComponent = () => (
+const LoginComponent = ( { onLoginClick, onRegisterClick } ) => (
     <div className={styles.loginDiv}>
-        this is from the login
+        <a href="#" onClick={onLoginClick}>{loginText}</a>
+        <a href="#" onClick={onRegisterClick}>{registerText}</a>
     </div>
 );
+
+LoginComponent.propTypes = {
+    onLoginClick: PropTypes.func.isRequired,
+    onRegisterClick: PropTypes.func.isRequired
+};
 
 export default LoginComponent;
