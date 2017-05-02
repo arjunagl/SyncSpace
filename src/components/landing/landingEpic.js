3d581cabad185b69c84015b76ee5c35e5a0da8d7
AGL\A136836 <amarambe@agl.com.au>
2017-05-03 09:10:48 +1000

    Got flatmap working, dont know how.

--- src/components/landing/landingEpic.js
+++ src/components/landing/landingEpic.js
@@ -4,89 +4,56 @@ import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/concat';
 import 'rxjs/add/observable/of';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 
 /**
  * 
  * @param {*} action$ 
  */
-
 export const landingEpic = action$ => {
     console.log('generating epic...');
-    const applyingShoppingListSource = action$.ofType('APPLY_SHOPPING_LISTS').mapTo({ type: 'APPLYING_SHOPPING_LISTS' });
-    const appliedShoppingListSource = action$.ofType('APPLY_SHOPPING_LISTS').mapTo({
-        type: 'APPLIED_SHOPPING_LISTS',
-        AppliedShoppingLists: [{
-            ShoppingListId: '1',
-            Item: {
-                Id: '1',
-                Name: 'Apples'
-            },
-            Location: {
-                Isle: '1',
-                Description: 'Next to Oranges'
-            },
-        }, {
-            ShoppingListId: '1',
-            Item: {
-                Id: '1',
-                Name: 'Apples'
-            },
-            Location: {
-                Isle: '1',
-                Description: 'Next to Oranges'
-            }
-        },
-        {
-            ShoppingListId: '1',
-            Item: {
-                Id: '1',
-                Name: 'Apples'
-            },
-            Location: {
-                Isle: '1',
-                Description: 'Next to Oranges'
-            }
-        }]
-    }).delay(5000);
-
-    return applyingShoppingListSource.concat(appliedShoppingListSource);
+    return action$.ofType('APPLY_SHOPPING_LISTS')
+        .flatMap(() =>
+            Observable.concat(
+                // Fire 2 actions, one after the other
+                Observable.of({
+                    type: 'APPLYING_SHOPPING_LISTS'
+                }),
+                Observable.of({
+                    type: 'APPLIED_SHOPPING_LISTS',
+                    AppliedShoppingLists: [{
+                        ShoppingListId: '1',
+                        Item: {
+                            Id: '1',
+                            Name: 'Apples'
+                        },
+                        Location: {
+                            Isle: '1',
+                            Description: 'Next to Oranges'
+                        },
+                    }, {
+                        ShoppingListId: '1',
+                        Item: {
+                            Id: '1',
+                            Name: 'Apples'
+                        },
+                        Location: {
+                            Isle: '1',
+                            Description: 'Next to Oranges'
+                        }
+                    },
+                    {
+                        ShoppingListId: '1',
+                        Item: {
+                            Id: '1',
+                            Name: 'Apples'
+                        },
+                        Location: {
+                            Isle: '1',
+                            Description: 'Next to Oranges'
+                        }
+                    }]
+                }).delay(5000)
+            )
+        );
 };
-
-// export const landingEpic = action$ => (
-//     action$.ofType('APPLY_SHOPPING_LISTS').delay(5000).map(() => ({
-//         type: 'APPLIED_SHOPPING_LISTS',
-//         AppliedShoppingLists: [{
-//             ShoppingListId: '1',
-//             Item: {
-//                 Id: '1',
-//                 Name: 'Apples'
-//             },
-//             Location: {
-//                 Isle: '1',
-//                 Description: 'Next to Oranges'
-//             },
-//         }, {
-//             ShoppingListId: '1',
-//             Item: {
-//                 Id: '1',
-//                 Name: 'Apples'
-//             },
-//             Location: {
-//                 Isle: '1',
-//                 Description: 'Next to Oranges'
-//             }
-//         },
-//         {
-//             ShoppingListId: '1',
-//             Item: {
-//                 Id: '1',
-//                 Name: 'Apples'
-//             },
-//             Location: {
-//                 Isle: '1',
-//                 Description: 'Next to Oranges'
-//             }
-//         }]
-//     }))
-// );
