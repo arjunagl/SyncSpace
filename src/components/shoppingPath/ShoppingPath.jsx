1f3b997473f97b30d40c7ac708a2c146d030267b
AGL\A136836 <amarambe@agl.com.au>
2017-05-20 09:43:15 +1000

    Added the shopping path.

--- src/components/shoppingPath/ShoppingPath.jsx
+++ src/components/shoppingPath/ShoppingPath.jsx
@@ -20,32 +20,23 @@ const ShoppingPathComponent = ({ AppliedShoppingLists }) => {
         (shoppingPathItem) => (shoppingPathItem.Location.Name));
 
 
+    // eslint-disable-next-line max-len
     const shoppingPathToRender = Object.keys(sortedAndGroupedShoppingPathOnLocation).map(locationKey => {
         return (
-            <div key={locationKey}>
+            <div key={locationKey} className={styles.ShoppingPathLocation}>
                 Location: {locationKey}
                 {
                     sortedAndGroupedShoppingPathOnLocation[locationKey].map(shoppingItem => {
-                        return (<div key={shoppingItem.Item.Id}>{shoppingItem.Item.Name}</div>);
+                        return (
+                            <div className={styles.ShoppingItem} key={shoppingItem.Item.Id}>
+                                {shoppingItem.Item.Name}
+                            </div>
+                        );
                     })
                 }
             </div>);
     });
 
-    /*const shoppingPathToRender = AppliedShoppingLists.map(shoppingListItem =>
-        <div
-            key={shoppingListItem.Item.Id}
-            className={styles.ShoppingItem}
-        >
-            <input type="checkbox" id={shoppingListItem.Item.Id} />
-            <label
-                htmlFor={shoppingListItem.Item.Id}
-            >
-                {shoppingListItem.Item.Name} <br />
-                Location: {shoppingListItem.Location.Name}, {shoppingListItem.Location.Description}
-            </label>
-        </div>
-    );*/
     return (
         <div>
             {shoppingPathToRender}
